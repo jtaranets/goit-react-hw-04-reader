6d7e3b162cb6c152d4375a43d23952d717ff818c
Julia Taranets <jtaranets@gmail.com>
2019-11-28 00:08:14 +0200

    Updates

---
+++ precache-manifest.07181e2607f68f456e1b56f796cdda0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d26bb12dd144ad4e7a0231aa902bb7",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "8769de1b67a601a85edc",
+    "url": "/goit-react-hw-04-reader/static/css/main.f0b0f545.chunk.css"
+  },
+  {
+    "revision": "e3ff48deed326c073b1f",
+    "url": "/goit-react-hw-04-reader/static/js/2.3695a1eb.chunk.js"
+  },
+  {
+    "revision": "8769de1b67a601a85edc",
+    "url": "/goit-react-hw-04-reader/static/js/main.9970dc01.chunk.js"
+  },
+  {
+    "revision": "0589309077f042b3f1b0",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.da974f8d.js"
+  }
+]);
